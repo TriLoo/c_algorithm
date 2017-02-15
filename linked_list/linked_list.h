12bacf7b353189e78d4e381f74e51f62845a5439
smh <song_mh@yeah.net>
2017-02-15 19:57:49 +0800

    add struct difination

--- linked_list/linked_list.h
+++ linked_list/linked_list.h
@@ -1,8 +1,14 @@
 #ifndef __LINKED_LIST_H__
 #define __LINKED_LIST_H__
 
+struct node;
 typedef struct node* ptrToList;
 
+struct node 
+{
+	int num;
+	ptrToList next;
+};
 
-
+ptrToList MakeEmpty(ptrToList L);
 #endif
