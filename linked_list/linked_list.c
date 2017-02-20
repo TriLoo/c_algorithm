eff3f3794b00a5aa5747e46cdb1265df392392c6
smh <song_mh@yeah.net>
2017-02-20 19:03:33 +0800

    complete some functions

--- linked_list/linked_list.c
+++ linked_list/linked_list.c
@@ -1,11 +1,91 @@
 #include "linked_list.h"
 
-ptrToList MakeEmpty(ptrToList L)
+/* test wether the list is empty*/
+
+int IsEmpty(List L)
 {
-	return L;
+	/*
+	if(L->next == NULL)
+		return 1;
+	else
+		return 0;
+		*/
+	return L->next == NULL;
 }
 
-int main(int argc, char **argv)
+/* test the P whether is the last element
+ * here, the L is unused.
+ * */
+int IsLast(Position P, List L)
 {
-	return 0;
+	return P->next==NULL;
 }
+
+/* function find, return the position of to be found element*/
+Position Find(int x, List L)
+{
+	Position tmp = L->next;
+	if(L->num == x)
+		tmp = L;
+	else
+	{
+		while (tmp != NULL && tmp->num != x)
+		{
+			tmp = tmp->next;
+		}
+	}
+	return tmp;
+}
+
+/* Delete a element from the list*/
+void Delete(int x, List L)
+{
+	Position tmp;
+	Position P = FindPrevious(x, L);
+	if( !IsLast(P, L))
+	{
+		tmp = P->next;
+		P->next = tmp->next;
+		free(tmp);
+	}
+}
+
+/* Find the prevoius element to be found element */
+Position FindPrevious(int x, List L)
+{
+	Position P = L;
+	while(P->next != NULL && P->next->num != x)
+	{
+		P = P->next;
+	}
+
+	return P;
+	//Position tmp = L;
+	/*
+	if(L == x)
+		tmp = L;
+	else
+	{
+		while(P->next != NULL && P->num != x)
+		{
+			tmp = P;
+			P = P->next;
+		}
+	}
+	return tmp;
+	*/
+}
+
+/* Insert an element to List*/
+void Insert(int x, List L, Position P)
+{
+	Position tmp;
+	
+	tmp = (struct node)malloc(sizeof(struct node));
+	if(tmp == NULL)
+		FatalError("Out of space!!");
+	tmp->num = x;
+	tmp->next = P->next;
+	P->next = tmp;
+}
+
