88993342f26a0847f6a1c60cb61ae58b7caca552
smh <song_mh@yeah.net>
2017-12-09 23:25:17 +0800

    add momentum sgd

--- MXNetChapter6/momentumS.py
+++ MXNetChapter6/momentumS.py
@@ -4,4 +4,7 @@ from mxnet import ndarray as nd
 from mxnet import gluon
 import random
 
-
+def sgd_momentum(params, vs, lr, mom, batch_size):
+    for param, v in zip(params, vs):
+        v[:] = mom * v + lr * param.grad / batch_size
+        param[:] -= v
