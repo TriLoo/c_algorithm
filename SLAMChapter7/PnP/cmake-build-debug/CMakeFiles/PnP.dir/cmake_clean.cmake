fdd58fe94a805721ace2ae0de07484bb695fa971
smh <song_mh@yeah.net>
2017-12-29 22:06:36 +0800

    initial submmit of PnP

---
+++ SLAMChapter7/PnP/cmake-build-debug/CMakeFiles/PnP.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PnP.dir/main.cpp.o"
+  "PnP.pdb"
+  "PnP"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PnP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
