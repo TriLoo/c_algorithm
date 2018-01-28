6c10112b9b5a00cba41a87b4c6854bb9feba936e
smh <song_mh@yeah.net>
2018-01-28 23:47:04 +0800

    update

--- GluonAlexNet/main.py
+++ GluonAlexNet/main.py
@@ -3,6 +3,8 @@ from mxnet import gluon
 from mxnet import autograd
 from mxnet import ndarray as nd
 import os
+import utils
+from mxnet import image
 
 # Define the model
 class AlexNet(gluon.nn.Block):
@@ -38,22 +40,69 @@ class AlexNet(gluon.nn.Block):
     def forward(self, x):
         return nd.softmax(self.net(x))
 
-
 # define Train Function
-def train():
-    print('Training ...')
+def train(train_data, test_data, net, batch_size, lr, mom, epochs, period):
+    assert period >= batch_size and period % batch_size == 0
+    net.collect_params().initialize(mx.init.Normal(sigma=0.01), force_reinit=True)
 
 # define training target
 net = AlexNet()
 
-# load data  OR  Train
+# Prepare the trainning data
+def apply_aug_lists(img, augs):
+    for aug in augs:
+        img = aug(img)
+    return img
+
+def get_transform(augs):
+    def transform(data, label):
+        data = data.astype('float32')
+        if augs is not None:
+            data = nd.stack(*[apply_aug_lists(d, augs) for d in data])
+        data = nd.transpose(data, (0, 3, 1, 2))
+        return data, label.astype('float32')
+    return transform
+
+def transformTest(data, label, augs_):
+    data = data.astype('float32')
+    for aug in augs_:
+        data = aug(data)
+    data = nd.transpose(data, (2, 0, 1))
+    return data, nd.array([label]).asscalar().astype('float32')
+
+batch_size = 32     # set 8 to test
+augs = [
+    image.HorizontalFlipAug(.5),
+    image.CenterCropAug((224, 224))
+    #image.RandomCropAug((224, 224))
+]
+
+#train_set = gluon.data.vision.ImageFolderDataset(root='~/.mxnet/datasets/oxford102/train_datas', transform=get_transform(augs))
+#train_set = gluon.data.vision.ImageFolderDataset(root='../data/hotdog/train', transform=get_transform(augs))
+train_set = gluon.data.vision.ImageFolderDataset(root='~/.mxnet/datasets/oxford102/train_datas', transform=lambda X, y: transformTest(X, y, augs))
+test_set = gluon.data.vision.ImageFolderDataset(root='~/.mxnet/datasets/oxford102/test_datas', transform=get_transform(augs))
+
+train_data = gluon.data.DataLoader(train_set, batch_size=batch_size, shuffle=True)
+test_data = gluon.data.DataLoader(test_set, batch_size=batch_size)
+
+for X,label in train_data:
+    X = X.transpose((0, 2, 3, 1)).clip(0, 255)/255
+    #X = X.transpose((0, 2, 3, 1)).clip(0, 255)/255
+    utils.show_images(X, 4, 8)
+    print(label)
+    break
+
+
+
+# Load parameters  OR  Train
 if os.path.exists('./AlexNet.params'):
-    net.load_params('AlexNet.params', allow_missing=False)
+    net.collect_params().load('AlexNet.params', allow_missing=False)
 else:
+    net.initialize(init=mx.init.Xavier())
     # Train
-    train()
+    # train()
     # save parameters
-    net.save_params('AlexNet.params')
+    # net.collect_params().save('AlexNet.params')
 
 # Test
 
