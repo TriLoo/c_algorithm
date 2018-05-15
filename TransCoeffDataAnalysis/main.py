9ac42d0e64e5fd2f793b13d0edb563eeceb92914
smh <song_mh@yeah.net>
2018-05-15 23:26:00 +0800

    update

--- TransCoeffDataAnalysis/main.py
+++ TransCoeffDataAnalysis/main.py
@@ -1,7 +1,12 @@
 import numpy as np
+import itertools
 import scipy.optimize
+import utils
+import matplotlib.pyplot as plt
+from matplotlib.legend_handler import  HandlerLine2D
 
-L = 1.
+#A = 1.
+L = 1e-5
 # Define object function
 def objectFunction(theta, datas, labels):
     '''
@@ -13,26 +18,64 @@ def objectFunction(theta, datas, labels):
     :return: the total errors between training labels and predictions
     '''
 
-    a, b, c, Is1, Is2, Is3 = theta
+    A, a, b, c, Is1, Is2, Is3 = theta
     global L
 
     totalError = 0.0
-    for x, y in zip(datas, labels):
+    #for x, y in zip(datas, labels):     # OR
+    for x, y in itertools.zip_longest(datas, labels):
         item1 = a / (1. + x / Is1)
         item2 = b * x / (1. + (x / Is2)**2)
         item3 = c * x**2 / (1. + (x / Is3)**3)
-        error = y - np.exp(-(item1 + item2 + item3) * L)
+        #error = np.abs(y - A * np.exp(-(item1 + item2 + item3) * L))
+        error = (y - A * np.exp(-(item1 + item2 + item3) * L))**2
         totalError = totalError + error
 
     return totalError
 
+def predict(theta, x):
+    A, a, b, c, Is1, Is2, Is3 = theta
+    global L
+
+    item1 = a / (1. + x / Is1)
+    item2 = b * x / (1. + (x / Is2)**2)
+    item3 = c * x**2 / (1. + (x / Is3)**3)
+
+    y = A * np.exp(-(item1 + item2 + item3) * L)
+    return y
+
+
 def otherOptimization(func, theta0, datas, labels):
-    result = scipy.optimize.minimize(func, thetah0, (datas, labels), method='Powell')
+    result = scipy.optimize.minimize(func, theta0, (datas, labels), method='Powell', options={'maxiter':100})
+    #result = scipy.optimize.minimize(func, theta0, (datas, labels), method='Nelder-Mead')
     return result
 
-a = [1, 2, 3, 4]
-b = [9, 8, 7, 6]
+datas, labels = utils.readData('datas.xlsx')
 
-for x, y in zip(a, b):
-    print('a = ', x)
-    print('b = ', y)
+datas = np.array(datas)
+labels = np.array(labels)
+
+#theta0 = (.2, 200., 3., 1., .01, 20., 500.0)
+theta0 = (.2, 100000., 100., 1., 0.01, 20., 500.0)    # Try so many initializations, total bad
+
+result = otherOptimization(objectFunction, theta0, datas, labels)
+print(result)
+
+minVal = np.min(datas)
+maxVal = np.max(datas)
+
+theta = [a for a in result.x]
+print(theta)
+
+testDatas = np.array(np.linspace(minVal, maxVal, 2000))
+testOut = [predict(theta, x) for x in testDatas]
+
+fig = plt.figure(1)
+plotf, =  plt.plot(datas, labels, label='Origin')
+plotfi, =  plt.plot(testDatas, testOut, label='Predicted')
+
+plt.legend(handler_map={plotf:HandlerLine2D(numpoints=4)})
+plt.scatter(datas.reshape((-1)), labels, marker='x', color='g')
+
+plt.show()
+
