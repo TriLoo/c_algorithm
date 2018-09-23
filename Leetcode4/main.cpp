d0d10a1a848ed4641fcf4e25b117256db98e0f7c
smh <song_mh@yeah.net>
2018-09-23 23:54:41 +0800

    init commit leetcode4, median in two sorted array

---
+++ Leetcode4/main.cpp
@@ -0,0 +1,23 @@
+/**
+ * @author smh
+ * @date 2018.09.23
+ *
+ * @brief Leetcode 4
+ *   Median of Two Sorted Arrays
+ *
+ *      思路： 根据中位数定义，当元素的个数为奇数时，中位数为中间的数；若元素个数为偶数时，中位数为中间的两个数的均值
+ */
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2)
+{
+}
+
+int main() {
+
+
+    return 0;
+}
