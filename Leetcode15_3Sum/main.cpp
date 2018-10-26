09e96590a1224f4d0f480f8b823bb0f07ef8b909
smh <song_mh@yeah.net>
2018-10-26 23:38:13 +0800

    init commit, leetcode 15

---
+++ Leetcode15_3Sum/main.cpp
@@ -0,0 +1,27 @@
+/**
+ * @author smh
+ * @date 2018.10.25
+ *
+ * @brief Leetcode 15 Medium
+ *   3 Sum: Find all unique triplets in the array which gives the sum of zero.
+ *
+ *   Notes:
+ */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<vector<int>> threeSum(vector<int> &nums)
+{
+    vector<vector<int>> ret;
+    sort(nums.begin(), nums.end()); // 从小到大
+
+
+}
+
+int main() {
+
+    return 0;
+}
