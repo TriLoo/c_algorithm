2897fc8dd8cadfa44a7ee7dd1ae84fa49189bbfb
smh <song_mh@yeah.net>
2018-11-10 22:56:41 +0800

    init commit leetcode 16

---
+++ Leetcode16_3SumClosest/main.cpp
@@ -0,0 +1,29 @@
+/**
+ * @author smh
+ * @date 2018.11.05
+ *
+ * @brief Leetcode 16, Medium
+ *   3Sum Closest
+ *
+ *   Return the sum of the three integers that the sum is cloest to 'target'.
+ *   Notes:
+ *     1.
+ */
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int threeSumClosest(vector<int>& nums, int target)
+{
+    int ret = 0;
+
+    return ret;
+}
+
+int main() {
+    // Under test
+
+    return 0;
+}
+
